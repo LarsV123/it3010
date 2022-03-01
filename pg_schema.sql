8a21964f4e08b379cbff5aecda5f8ec24eed5245
LarsV123 <42863501+LarsV123@users.noreply.github.com>
2022-03-01 17:06:55 +0100

    Add no-index version of Postgres experiment

--- pg_schema.sql
+++ pg_schema.sql
@@ -1,8 +1,10 @@
 -- Schema for the PostgreSQL version of the experiment
 -- As date/time is not used, we don't need to convert it to a better format.
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS trackpoint;
-CREATE TABLE trackpoint (
+DROP TABLE IF EXISTS trackpoint_no_index;
+DROP TABLE IF EXISTS trackpoint_indexed;
+-- Unindexed version
+CREATE TABLE trackpoint_no_index (
   tp_id SERIAL PRIMARY KEY,
   tp_user INTEGER NOT NULL,
   tp_point GEOMETRY NOT NULL,
@@ -10,5 +12,14 @@ CREATE TABLE trackpoint (
   tp_date VARCHAR(32),
   tp_time VARCHAR(32)
 );
-CREATE INDEX trackpoint_geom_index ON trackpoint USING GIST (tp_point);
+-- Indexed version
+CREATE TABLE trackpoint_indexed (
+  tp_id SERIAL PRIMARY KEY,
+  tp_user INTEGER NOT NULL,
+  tp_point GEOMETRY NOT NULL,
+  tp_altitude FLOAT,
+  tp_date VARCHAR(32),
+  tp_time VARCHAR(32)
+);
+CREATE INDEX trackpoint_geom_index ON trackpoint_indexed USING GIST (tp_point);
 COMMIT TRANSACTION;
