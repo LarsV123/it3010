1cfa57987b1d43929895bdd4f749e8d2324b2039
cdaxell <cdaxell@stud.ntnu.no>
2022-04-01 11:42:25 +0200

    Change

--- graph_plot.py
+++ graph_plot.py
@@ -28,6 +28,21 @@ for dbMean, dbStd in zip(mean.groupby("dbms"), std.groupby("dbms")):
     plt.errorbar(x, y, yerr=y_err, label=dbMean[0], fmt='o-')
 
 
+#Ditt just indexed
+data = pd.read_csv("results.csv")
+data = data[data.indexed == 1]
+mean = data.groupby(["dbms", "batch_size"]).mean().reset_index()
+std = data.groupby(["dbms", "batch_size"]).std().reset_index()
+for dbMean, dbStd in zip(mean.groupby("dbms"), std.groupby("dbms")):
+    print(dbMean)
+    print(dbStd)
+    x = dbMean[1]["batch_size"]
+    y = dbMean[1][y_var]
+    y_err = dbStd[1][y_var]
+    plt.errorbar(x, y, yerr=y_err, label=dbMean[0], fmt='o--')
+
+
+
 #Font and size
 font = {'size'   : 12}
 plt.rc('font', **font)
